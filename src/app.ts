b62567567c2431f970e6aab3519714d0d6eea8f2
HirotoUechi8 <h.uechi@buysell-technologies.com>
2023-04-25 18:56:11 +0900

    サーバーとレシーバーの設定をした

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { App } from "@slack/bolt";
+import { App, ExpressReceiver } from "@slack/bolt";
 import { config } from "dotenv";
 import express from "express";
 
@@ -10,8 +10,18 @@ export const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 const server = express();
 const port = process.env.PORT || 3000;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use('/slack/events', receiver.router);
+server.use('/slack/commands', receiver.router);
+server.use('/slack/actions', receiver.router);
+server.use('/slack/shortcuts', receiver.router);
+server.use('/slack/modals', receiver.router);
 
 server.listen(port, () => {
   console.log(`⚡️ Bolt app is running on port ${port}!`);
