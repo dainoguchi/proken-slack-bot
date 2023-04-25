da4619a7c0a993391b4bef2fd7dd32376ede3fc5
HirotoUechi8 <h.uechi@buysell-technologies.com>
2023-04-25 18:27:09 +0900

    port指定してみた

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { App } from "@slack/bolt";
 import { config } from "dotenv";
+import express from "express";
 
 config();
 
@@ -7,4 +8,11 @@ export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
+});
+
+const server = express();
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`⚡️ Bolt app is running on port ${port}!`);
 });
