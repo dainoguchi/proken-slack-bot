c2be96b7970c571342f6dbeabcad7401a4d60e7b
MandeNaren <y2pgftpe@gmail.com>
2023-04-26 16:28:10 +0900

    socketmodeの修正

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import { App } from '@slack/bolt'
 
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+
+  // ローカルで動かす場合はsocketModeをtrueにする
+  // socketMode: true,
+  // appToken: process.env.SLACK_APP_TOKEN,
 })
