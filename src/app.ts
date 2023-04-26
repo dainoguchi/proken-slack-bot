67b9078516df2287d3d11ad1bbae16f195b8c8af
ItsukiBaba <it.baba@buysell-technologies.com>
2023-04-26 10:15:09 +0900

    devのapp.tsに戻す

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,11 @@
-import { App, ExpressReceiver } from '@slack/bolt'
+import { App } from '@slack/bolt'
 import { config } from 'dotenv'
-import express from 'express'
 
 config()
 
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-})
-
-const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-})
-const server = express()
-const port = process.env.PORT || 3000
-
-server.use(express.urlencoded({ extended: true }))
-server.use(express.json())
-
-server.use('/slack/events', receiver.router)
-server.use('/slack/commands', receiver.router)
-server.use('/slack/actions', receiver.router)
-server.use('/slack/shortcuts', receiver.router)
-server.use('/slack/modals', receiver.router)
-
-server.listen(port, () => {
-  console.log(`⚡️ Bolt app is running on port ${port}!`)
 })
