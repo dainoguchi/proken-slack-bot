dc2f7c231e50a80085893de3cc10b903f1e1a5d6
MandeNaren <y2pgftpe@gmail.com>
2023-04-27 23:47:39 +0900

    HomeViewの中身を作成

--- src/home.ts
+++ src/home.ts
@@ -17,15 +17,151 @@ export const homeView: View = {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: 'SlackBuddyへようこそ',
+        text: 'このアプリはBuySellBuddyをSlackで利用できるようにしたものです:sparkles:\n\nBuySellBuddyとは、OpenAI社の先進的なAI技術を活用して、安全に人工知能と会話ができるサービスです。\n\nSlackBuddyを使うことで、あなたは日常的な質問から専門的なトピックまで、幅広い会話をSlack上で手軽に行うことができます。\nたとえば、アイデアのブレインストーミングや情報検索、問題解決のサポートなど、さまざまな目的で活用できます。\nSlackBuddyがあなたにとって最高の相棒となりますように！:sparkles:',
       },
     },
     {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: '何でも質問することができます',
+        text: 'SlackBuddyの使い方を紹介します。\n',
       },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '\n',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*前提*',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'SlackBuddyへの質問は「SlackBuddyとのダイレクトメッセージ」か「チャンネル」で可能です。個人間のダイレクトメッセージで利用する場合はSlackBuddyを招待する必要があります。',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '\n',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*使い方*',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*1️⃣ テンプレートを使う方法*',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*@BuySellGPT* とメンションします',
+      },
+    },
+    {
+      type: 'image',
+      image_url:
+        'https://storage.googleapis.com/slack-buddy-usage/template-text1.png',
+      alt_text: 'inspiration',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*テンプレート一覧を見る* を押下すると質問テンプレートを選択できます。入力ガイドに従って質問してみましょう！',
+      },
+    },
+    {
+      type: 'image',
+      image_url:
+        'https://storage.googleapis.com/slack-buddy-usage/templete-text2.png',
+      alt_text: 'inspiration',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '\n',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*2️⃣ 自分の言葉で質問する方法*',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*@BuySellGPT* とメンションし、続けて質問文を入力して送信すると、スレッドに回答が返ってきます。テンプレートを使いたくない時はこちらの方法で質問してみましょう！',
+      },
+    },
+    {
+      type: 'image',
+      title: {
+        type: 'plain_text',
+        text: 'image1',
+        emoji: true,
+      },
+      image_url:
+        'https://storage.googleapis.com/slack-buddy-usage/free-text1.png',
+      alt_text: 'image1',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '回答がスレッドに返ってきます。',
+      },
+    },
+    {
+      type: 'image',
+      title: {
+        type: 'plain_text',
+        text: 'image1',
+        emoji: true,
+      },
+      image_url:
+        'https://storage.googleapis.com/slack-buddy-usage/free-text1.5.png',
+      alt_text: 'image1',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'スレッド内では会話の文脈を考慮して返答してくれます。',
+      },
+    },
+    {
+      type: 'image',
+      title: {
+        type: 'plain_text',
+        text: 'image1',
+        emoji: true,
+      },
+      image_url:
+        'https://storage.googleapis.com/slack-buddy-usage/free-text2.png',
+      alt_text: 'image1',
     },
   ],
 }
